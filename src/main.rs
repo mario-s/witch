ce06a4f823c419873dfb75489863ccc90de3d744
Mario Schroeder <mario.schroeder@springernature.com>
2017-10-24 16:32:36 +0200

    changed sample to glutin

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,51 @@
-extern crate piston_window;
 extern crate find_folder;
+extern crate piston;
+extern crate glutin_window;
+extern crate graphics;
+extern crate opengl_graphics;
+
 
 use std::path::PathBuf;
-use piston_window::*;
-use piston_window::text::Text;
+use piston::window::WindowSettings;
+use piston::event_loop::*;
+use piston::input::*;
+use glutin_window::GlutinWindow as Window;
+use opengl_graphics::{ GlGraphics, OpenGL, GlyphCache, TextureSettings};
+use graphics::*;
 
 
-fn new_window() -> PistonWindow {
-    let opengl = OpenGL::V3_2;
-    return WindowSettings::new("Hello Piston!", [640, 480])
-        .opengl(opengl)
-        .exit_on_esc(true)
-        .build().unwrap();
+
+struct Canvas {
+    gl: GlGraphics
+}
+
+impl Canvas {
+    fn new(opengl: OpenGL) -> Canvas {
+        Canvas{gl: GlGraphics::new(opengl)}
+    }
+
+    fn render(&mut self, args: RenderArgs) {
+        const BLACK: [f32; 4] = [0.0, 0.0, 0.0, 1.0];
+        const RED:   [f32; 4] = [1.0, 0.0, 0.0, 1.0];
+        let viewport = args.viewport();
+        let mut cache = GlyphCache::new(assets("FreeSans.ttf"), (), TextureSettings::new()).unwrap();
+
+        self.gl.draw(viewport, |context, gl| {
+            clear([1.0, 1.0, 1.0, 1.0], gl);
+            // render code here
+            rectangle(RED, [10.0, 10.0, 50.0, 80.0],
+                      context.transform,
+                      gl);
+
+            text(BLACK, 20, "Test", &mut cache, context.transform.trans(150.0, 80.0), gl);
+        });
+    }
+
+    fn update(&mut self, args: UpdateArgs) {
+        // update code here
+
+    }
+
 }
 
 fn assets(path: &str) -> PathBuf {
@@ -21,46 +55,27 @@ fn assets(path: &str) -> PathBuf {
 }
 
 fn main() {
-    let black = [0.0, 0.0, 0.0, 1.0];
-    let red = [1.0, 0.0, 0.0, 0.8];
+    let opengl = OpenGL::V3_2;
 
-    let mut window: PistonWindow = new_window();
+    let mut window: Window = WindowSettings::new(
+        "Demo",
+        [640, 480]
+    )
+        .opengl(opengl)
+        .exit_on_esc(true)
+        .build()
+        .unwrap();
 
-    let settings = TextureSettings::new();
-    let mut glyphs = Glyphs::new(assets("FreeSans.ttf"), window.factory.clone(), settings).unwrap();
 
+    let mut canvas = Canvas::new(opengl);
+    let mut events = Events::new(EventSettings::new());
+    while let Some(e) = events.next(&mut window) {
+        if let Some(r) = e.render_args() {
+            canvas.render(r)
+        }
 
-    while let Some(event) = window.next() {
-        window.draw_2d(&event, |context, graphics| {
-            clear([1.0; 4], graphics);
-
-            rectangle(red, [10.0, 10.0, 50.0, 80.0],
-                      context.transform,
-                      graphics);
-
-            Text::new_color(black, 70)
-                .draw(
-                    &"P",
-                    &mut glyphs,
-                    &context.draw_state,
-                    context.transform.trans(15.0, 80.0),
-                    graphics);
-
-            Text::new_color(black, 50)
-                .draw(
-                    &"iston Tutorial",
-                    &mut glyphs,
-                    &context.draw_state,
-                    context.transform.trans(60.0, 80.0),
-                    graphics);
-
-            Text::new_color(black, 15)
-                .draw(
-                    &"<ESC> to exit",
-                    &mut glyphs,
-                    &context.draw_state,
-                    context.transform.trans(540.0, 470.0),
-                    graphics);
-        });
+        if let Some(u) = e.update_args() {
+            canvas.update(u);
+        }
     }
 }
