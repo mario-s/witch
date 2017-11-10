e68a4fbb0fadc1de2f4dd8e6e6c958ac205c544d
Mario Schroeder <mario.schroeder@springernature.com>
2017-11-10 14:14:53 +0100

    moved constants

--- src/game/canvas.rs
+++ src/game/canvas.rs
@@ -14,14 +14,15 @@ pub struct Canvas {
 }
 
 impl Canvas {
+    const BLACK: [f32; 4] = [0.0, 0.0, 0.0, 1.0];
+    const RED:   [f32; 4] = [1.0, 0.0, 0.0, 1.0];
+
     pub fn new(opengl: OpenGL) -> Canvas {
         Canvas{gl: GlGraphics::new(opengl)}
     }
 
     #[allow(unused_must_use)]
     pub fn render(&mut self, args: RenderArgs) {
-        const BLACK: [f32; 4] = [0.0, 0.0, 0.0, 1.0];
-        const RED:   [f32; 4] = [1.0, 0.0, 0.0, 1.0];
         let viewport = args.viewport();
         let mut cache = GlyphCache::new(Assets::assets("FreeSans.ttf"), (), TextureSettings::new()).unwrap();
         let back_texture = Assets::texture("parallax-forest-back-trees.png");
@@ -37,12 +38,11 @@ impl Canvas {
             image(&light_texture, context.transform, gl);
             image(&front_texture, context.transform, gl);
 
-
-            rectangle(RED, [20.0, 20.0, 50.0, 80.0],
+            rectangle(Canvas::RED, [20.0, 20.0, 50.0, 80.0],
                       context.transform,
                       gl);
 
-            text(BLACK, 30, &"Blair Witch", &mut cache, context.transform.trans(100.0, 90.0), gl);
+            text(Canvas::BLACK, 30, &"Blair Witch", &mut cache, context.transform.trans(100.0, 90.0), gl);
         });
     }
 
