a0b109b3495195951f3555e56da63331594c0f45
Mario Schroeder <mario.schroeder@springernature.com>
2017-11-24 13:12:30 +0100

    added background translation

--- src/game/canvas.rs
+++ src/game/canvas.rs
@@ -11,22 +11,13 @@ use opengl_graphics::{ GlGraphics, OpenGL, GlyphCache, TextureSettings, Texture}
 use graphics::*;
 use game::assets::Assets;
 
-
-pub struct Canvas {
-    gl: GlGraphics,
-    translation: f64,
-    backgrounds: [Texture; 4]
+struct Env {
+    backgrounds: [Texture; 4],
 }
 
-
-impl Canvas {
-    const BLACK: [f32; 4] = [0.0, 0.0, 0.0, 1.0];
-    const RED:   [f32; 4] = [1.0, 0.0, 0.0, 1.0];
-
-    pub fn new(opengl: OpenGL) -> Canvas {
-        Canvas{
-            gl: GlGraphics::new(opengl),
-            translation: 0.0,
+impl Env {
+    fn new() -> Env {
+        Env {
             backgrounds:  [
                 Assets::texture("parallax-forest-back-trees.png"),
                 Assets::texture("parallax-forest-middle-trees.png"),
@@ -36,22 +27,62 @@ impl Canvas {
         }
     }
 
+    fn witch() -> Sprite<Texture> {
+        let witch = Rc::new(Assets::icon("witch-icon.png"));
+        let mut sprite = Sprite::from_texture(witch.clone());
+        sprite.set_position(50.0, 80.0);
+        return sprite;
+    }
+}
+
+
+pub struct Canvas {
+    gl: GlGraphics,
+    env: Env,
+    translation: f64,
+    backgroundTranslations: [f64; 4],
+}
+
+
+impl Canvas {
+    const BLACK: [f32; 4] = [0.0, 0.0, 0.0, 1.0];
+    const RED:   [f32; 4] = [1.0, 0.0, 0.0, 1.0];
+    const ZERO: f64 = 0.0;
+
+    pub fn new(opengl: OpenGL) -> Canvas {
+        Canvas{
+            gl: GlGraphics::new(opengl),
+            env: Env::new(),
+            translation: 0.0,
+            backgroundTranslations: [0.0,0.0,0.0,0.0],
+        }
+    }
+
     #[allow(unused_must_use)]
     pub fn render(&mut self, args: RenderArgs) {
         let viewport = args.viewport();
+
+        let imgs = &self.env.backgrounds;
+
+        let sprite = Env::witch();
+        let mut scene = Scene::new();
+        scene.add_child(sprite);
+
         let mut cache = GlyphCache::new(Assets::assets("FreeSans.ttf"), (), TextureSettings::new()).unwrap();
 
-        let mut scene = Scene::new();
-        scene.add_child(Canvas::witch());
-
         let translation = self.translation;
-        let imgs = &self.backgrounds;
+        let translations = self.backgroundTranslations;
+        let mut index = 0;
 
         self.gl.draw(viewport, |context, gl| {
             clear([1.0, 1.0, 1.0, 1.0], gl);
 
             for texture in imgs.into_iter() {
-                image(texture, context.transform, gl);
+                let translation = translations[index];
+                let width: f64 = texture.get_width() as f64;
+                image(texture, context.transform.trans(translation, 0.0), gl);
+                image(texture, context.transform.trans(width + translation, 0.0), gl);
+                index += 1;
             }
 
             let trans = context.transform.trans(translation, 0.0);
@@ -62,15 +93,19 @@ impl Canvas {
     }
 
     pub fn update(&mut self, args: UpdateArgs) {
-        // update code here
-        self.translation += 0.1;
+        self.translation += 0.5;
+
+        self.backgroundTranslations[0] -= 0.01;
+        self.backgroundTranslations[1] -= 0.02;
+        self.backgroundTranslations[3] -= 0.04;
+
+        let min: f64 = -1.0 * self.env.backgrounds[0].get_width() as f64;
+        for i in 0..4 {
+            if self.backgroundTranslations[i] < min {
+                self.backgroundTranslations[i] = Canvas::ZERO;
+            }
+        }
+
     }
 
-
-    fn witch() -> Sprite<Texture> {
-        let witch = Rc::new(Assets::icon("witch-icon.png"));
-        let mut sprite = Sprite::from_texture(witch.clone());
-        sprite.set_position(50.0, 80.0);
-        return sprite;
-    }
 }
